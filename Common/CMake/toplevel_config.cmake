3d61c83f60d4360271bfef19cb3945bf133bba99
Jacky <JackyCBN@163.com>
2020-04-08 08:22:53 +0800

    add google test

--- Common/CMake/toplevel_config.cmake
+++ Common/CMake/toplevel_config.cmake
@@ -1,6 +1,7 @@
 # Only do these if this is the main project, and not if it is included through add_subdirectory
 if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
 
+    message(STATUS "top level config")
 	enable_testing()
 	
     # Optionally set things like CMAKE_CXX_STANDARD, CMAKE_POSITION_INDEPENDENT_CODE here
@@ -23,5 +24,15 @@ if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
 	add_subdirectory(docs)
     else()
 	message(STATUS "Doxygen not found, not building docs")
+    endif()
+
+
+    # FetchContent added in CMake 3.11, downloads during the configure step
+    include(FetchContent)
+    # FetchContent_MakeAvailable was not added until CMake 3.14
+    message(STATUS "top level config111")
+    if(${CMAKE_VERSION} VERSION_LESS 3.14)
+        message(STATUS "top level config222")
+        include(add_FetchContent_MakeAvailable.cmake)
     endif()
 endif()
